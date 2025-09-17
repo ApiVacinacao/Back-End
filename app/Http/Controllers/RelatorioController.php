689ceaac09f0684487d95e79be5f6b22c42b6256
Rodrigo Alcantara <rodrigoinaldo281@gmail.com>
2025-09-17 09:53:24 -0300

    ajuste de relatorio

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -2,43 +2,51 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Carbon;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class RelatorioController
 {
     public function relatorioAgendamento(Request $request)
     {
+
         $query = \App\Models\Agendamento::query();
 
-        if($request->has('dataInicial') || $request->has('dataFinal')){
-            $dataInicial = Carbon::parse($request->input('dataInicial'));
-            $dataFinal = Carbon::parse($request->input('dataFinal'));
-
-            $query->whereBetween('data', [$dataInicial, $dataFinal]);
+        if($request->user_id)
+        {
+            $query->where('user_id', $request->user_id);
         }
 
-        //dd($query);
+        if($request->data_inicial && $request->data_final)
+        {
 
-        if($request->has('medico_id')){
-            $medico_id = $request->input('medico_id');
-            $query->where('medico_id', $medico_id);
+            $query->whereBetween('data',[$request->data_inicial, $request->data_fina]);
+
+        }elseif ($request->data_inicial) {
+
+            $query->whereDate('data', $request->data_inicial);
+
+        }elseif ($request->data_final){
+
+            $query->whereDate('data', $request->data_final);
         }
 
-        if($request->has('user_id')){
-            $user_id = $request->input('user_id');
-            $query->where('user_id', $user_id);
+
+        if($request->medico_id)
+        {
+            $query->where('medico_id', $request->medico_id);
         }
 
-        if($request->has('tipo_consulta_id')){
-            $tipo_consulta_id = $request->input('tipo_consulta_id');
-            $query->where('tipo_consulta_id', $tipo_consulta_id);
+        if($request->local_atendimento_id)
+        {
+            $query->where('local_atendimento_id', $request->local_atendimento_id);
         }
 
-        if($request->has('local_atendimento_id')){
-            $local_atendimento_id = $request->input('local_atendimento_id');
-            $query->where('local_atendimento_id', $local_atendimento_id);
+        if($request->tipo_consulta_id)
+        {
+            $query->where('tipo_consulta_id', $request->tipo_consulta_id);
         }
+
 
         $registro = $query->get();
         return response()->json($registro);
