34a184e998662a4f93a3d48f63251268d481ae7e
Rodrigo Alcantara <rodrigoinaldo281@gmail.com>
2025-10-16 11:18:25 -0300

    atualização  das validaçoes dos medicos

--- app/Http/Requests/StoreMedicoRequest.php
+++ app/Http/Requests/StoreMedicoRequest.php
@@ -19,7 +19,7 @@ class StoreMedicoRequest extends FormRequest
             'nome' => 'required|string|min:5|max:255|regex:/\s/',
             'cpf' => 'required|min:11|max:11|unique:medicos,cpf',
             'CRM' => 'required|string|unique:medicos,CRM|min:4|max:11|',
-            'especialidade_id' => 'required|number',
+            'especialidade_id' => 'required|exists:especialidades,id',
             'status' => 'required|boolean',
         ];
     }
@@ -27,28 +27,27 @@ class StoreMedicoRequest extends FormRequest
     public function messages(): array
     {
         return [
-            'nome.required' => 'O campo nome é obrigatório.',
-            'nome.string' => 'O campo nome deve ser uma string.',
-            'nome.min' => 'O campo nome deve ter no mínimo 5 caracteres.',
-            'nome.max' => 'O campo nome deve ter no máximo 255 caracteres.',
-            'nome.regex' => 'O campo nome deve conter pelo menos um espaço (nome e sobrenome).',
+            'nome.required' => 'O campo nome é obrigatório.', #ok
+            'nome.string' => 'O campo nome deve ser uma string.', #ok
+            'nome.min' => 'O campo nome deve ter no mínimo 5 caracteres.', #ok
+            'nome.max' => 'O campo nome deve ter no máximo 255 caracteres.', #ok
+            'nome.regex' => 'O campo nome deve conter pelo menos um espaço (nome e sobrenome).', #ok
 
-            'cpf.required' => 'O campo CPF é obrigatório.',
-            'cpf.min' => 'O CPF informado não é válido.',
-            'cpf.max' => 'O CPF informado não é válido.',
-            'cpf.unique' => 'Este CPF já está cadastrado.',
+            'cpf.required' => 'O campo CPF é obrigatório.', #ok
+            'cpf.min' => 'O CPF informado não é válido.', #ok
+            'cpf.max' => 'O CPF informado não é válido.', #ok
+            'cpf.unique' => 'Este CPF já está cadastrado.', #ok
 
-            'CRM.required' => 'O campo CRM é obrigatório.',
-            'CRM.string' => 'O campo CRM deve ser uma string.',
-            'CRM.unique' => 'Este CRM já está cadastrado.',
-            'CRM.min' => 'O campo CRM deve ter no mínimo 4 caracteres.',
-            'CRM.max' => 'O campo CRM deve ter no máximo 11 caracteres.',
+            'CRM.required' => 'O campo CRM é obrigatório.', #ok
+            'CRM.string' => 'O campo CRM deve ser uma string.', #ok
+            'CRM.unique' => 'Este CRM já está cadastrado.', #ok
+            'CRM.min' => 'O campo CRM deve ter no mínimo 4 caracteres.', #ok
+            'CRM.max' => 'O campo CRM deve ter no máximo 11 caracteres.', #ok
 
-            'especialidade_id.required' => 'O campo especialidade é obrigatório.',
+            'especialidade_id.required' => 'O campo especialidade é obrigatório.', #ok
+            'especialidade_id.exists' => 'A especialidade selecionada não existe.', #ok
 
-
-
-            'status.boolean' => 'O campo status deve ser verdadeiro ou falso.',
+            'status.boolean' => 'O campo status deve ser verdadeiro ou falso.', #ok
         ];
     }
 
--- app/Http/Requests/UpdateMedicoRequest.php
+++ app/Http/Requests/UpdateMedicoRequest.php
@@ -25,33 +25,33 @@ class UpdateMedicoRequest extends FormRequest
     {
         return [
             'nome' => 'sometimes|string|min:5|max:255|regex:/\s/',
-            'cpf' => 'sometimes|cpf|unique:medicos,cpf',
+            'cpf' => 'sometimes|min:11|max:11|unique:medicos,cpf',
             'CRM' => 'sometimes|string|unique:medicos,CRM|min:4|max:11|',
-            'especialidade_id' => 'sometimes|integer|exists:especialidades,id',
-            'status' => 'boolean',
+            'especialidade_id' => 'sometimes|exists:especialidades,id',
+            'status' => 'sometimes|boolean',
         ];
     }
 
         public function messages(): array
     {
         return [
-            'nome.string' => 'O campo nome deve ser uma string.',
-            'nome.min' => 'O campo nome deve ter no mínimo 5 caracteres.',
-            'nome.max' => 'O campo nome deve ter no máximo 255 caracteres.',
-            'nome.regex' => 'O campo nome deve conter pelo menos um espaço (nome e sobrenome).',
+            'nome.string' => 'O campo nome deve ser uma string.', #ok
+            'nome.min' => 'O campo nome deve ter no mínimo 5 caracteres.', #ok
+            'nome.max' => 'O campo nome deve ter no máximo 255 caracteres.', #ok
+            'nome.regex' => 'O campo nome deve conter pelo menos um espaço (nome e sobrenome).', #ok
 
-            'cpf.cpf' => 'O CPF informado não é válido.',
-            'cpf.unique' => 'Este CPF já está cadastrado.',
+            'cpf.min' => 'O CPF informado não é válido.', #ok
+            'cpf.max' => 'O CPF informado não é válido.', #ok
+            'cpf.unique' => 'Este CPF já está cadastrado.', #ok
 
-            'CRM.string' => 'O campo CRM deve ser uma string.',
-            'CRM.unique' => 'Este CRM já está cadastrado.',
-            'CRM.min' => 'O campo CRM deve ter no mínimo 4 caracteres.',
-            'CRM.max' => 'O campo CRM deve ter no máximo 11 caracteres.',
+            'CRM.string' => 'O campo CRM deve ser uma string.', #ok
+            'CRM.unique' => 'Este CRM já está cadastrado.', #ok
+            'CRM.min' => 'O campo CRM deve ter no mínimo 4 caracteres.', #ok
+            'CRM.max' => 'O campo CRM deve ter no máximo 11 caracteres.', #ok
 
-            'especialidade_id.required' => 'O campo especialidade é obrigatório.',
-            'especialidade_id.exists' => 'A especialidade selecionada não é válida.',
+            'especialidade_id.exists' => 'A especialidade selecionada não existe.', #ok
 
-            'status.boolean' => 'O campo status deve ser verdadeiro ou falso.',
+            'status.boolean' => 'O campo status deve ser verdadeiro ou falso.', #ok
         ];
     }
 
