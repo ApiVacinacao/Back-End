2e967f4f08a052315fea819411f41f5e9b21f290
rodrigo alcantara inaldo <rodrigoinaldo281@gmail.com>
2025-11-22 15:08:50 -0300

    route

--- routes/api.php
+++ routes/api.php
@@ -16,41 +16,30 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/esquecisenha', [AuthController::class,'esqueciaSenha']);
 
+//Route::get('/test', [testeEmail::class, 'senEmail']);
+
 Route::middleware([JwtMiddleware::class])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::post('/logout', [AuthController::class,'logout']);
+    Route::apiResource('medicos', MedicoController::class);
+    Route::apiResource('localAtendimentos', LocalAtendimentoController::class);
+    Route::apiResource('tipoConsultas', TipoConsultaController::class);
+    Route::apiResource('especialidades', EspecialidadeController::class);
 
-    // todos podem chamar estes:
-    Route::get('/meus_agendamentos', [AgendamentoController::class, 'show']);
+    Route::get('/users', [UserController::class, 'index']); 
+    Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::put('/users/{user}', [UserController::class, 'update']); 
+    Route::delete('/users/{user}', [UserController::class, 'destroy']); 
 
-    // ESTA ROTA AGORA É LIVRE PARA LOGADOS (o controller decide o que retorna)
-    Route::get('/agendamentos', [AgendamentoController::class, 'index']);
+    // rotas para alteração
+    Route::patch('/users/{user}', [UserController::class, 'toggleStatus']);
+    Route::patch('/medicos/{medico}/status', [MedicoController::class, 'toggleStatus']);
+    Route::patch('localAtendimentos/{id}/toggle-status', [LocalAtendimentoController::class, 'toggleStatus']);
+    Route::patch('tipoConsultas/{tipoConsulta}/toggle-status', [TipoConsultaController::class, 'toggleStatus']);
+    Route::patch('agendamentos/{agendamento}/toggle-status', [AgendamentoController::class, 'toggleStatus']);
 
-    // ações ADMIN
-    Route::middleware([RoleMiddleware::class . ':admin'])->group(function () {
-        Route::apiResource('agendamentos', AgendamentoController::class)
-            ->except(['index', 'show']); // <-- só admin edita, cria, deleta
-    });
-   
-    // 🔐 somente ADMIN
-    Route::middleware([RoleMiddleware::class . ':admin'])->group(function () {
+    Route::apiResource('agendamentos', AgendamentoController::class)->except(['show']);
+    Route::get('meus_agendamentos', [AgendamentoController::class, 'show']);
 
-        Route::apiResource('medicos', MedicoController::class);
-        Route::apiResource('localAtendimentos', LocalAtendimentoController::class);
-        Route::apiResource('tipoConsultas', TipoConsultaController::class);
-        Route::apiResource('especialidades', EspecialidadeController::class);
-
-        Route::get('/users', [UserController::class, 'index']);
-        Route::get('/users/{user}', [UserController::class, 'show']);
-        Route::put('/users/{user}', [UserController::class, 'update']);
-        Route::delete('/users/{user}', [UserController::class, 'destroy']);
-        Route::patch('/users/{user}', [UserController::class, 'toggleStatus']);
-
-        Route::patch('/medicos/{medico}/status', [MedicoController::class, 'toggleStatus']);
-        Route::patch('localAtendimentos/{id}/toggle-status', [LocalAtendimentoController::class, 'toggleStatus']);
-        Route::patch('tipoConsultas/{tipoConsulta}/toggle-status', [TipoConsultaController::class, 'toggleStatus']);
-        Route::patch('agendamentos/{agendamento}/toggle-status', [AgendamentoController::class, 'toggleStatus']);
-
-        Route::post('relatorios/agendamentos', [RelatorioController::class, 'relatorioAgendamento']);
-    });
+    Route::post('relatorios/agendamentos', [RelatorioController::class, 'relatorioAgendamento']);
 });
